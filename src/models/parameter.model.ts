1d2c975a3b289899150ac45a8b088455c35f2d9a
Maximilinano Osorio <mosorio@isi.edu>
2021-10-01 09:31:52 -0300

    fix: allow null fields

--- src/models/parameter.model.ts
+++ src/models/parameter.model.ts
@@ -34,38 +34,45 @@ export class Parameter extends Entity {
 
   @property({
     type: 'string',
+    nullable: true
   })
   unit?: string;
 
   @property({
     type: 'string',
+    nullable: true
   })
   unit_description?: string;
 
   @property({
     type: 'string',
     description: "Describes whether the data values will be categorical, ordered, or numerical",
+    nullable: true
   })
   data_type?: string;
 
   @property({
     type: 'any',
+    nullable: true
   })
   default?: any;
 
   @property({
     type: 'array',
     itemType: 'string',
+    nullable: true
   })
   choices?: string[];
 
   @property({
     type: 'number',
+    nullable: true
   })
   min?: number;
 
   @property({
     type: 'number',
+    nullable: true
   })
   max?: number;
 
@@ -77,6 +84,7 @@ export class Parameter extends Entity {
 
   @property({
     type: 'string',
+    nullable: true
   })
   prefix?: string;
 

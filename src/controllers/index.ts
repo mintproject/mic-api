35780fb6f906b9e72464a7cf2699089a4cf772ee
Maximilinano Osorio <mosorio@isi.edu>
2021-10-01 11:02:33 -0300

    Revert "add parameter controller"
    This reverts commit 4242fb54f5fd2d3a404a1c5baba3b8c394487aff.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,4 +3,3 @@ export * from './model-parameter.controller';
 export * from './model-directive.controller';
 export * from './model-input.controller';
 export * from './model.controller';
-export * from './parameter.controller';

4242fb54f5fd2d3a404a1c5baba3b8c394487aff
Maximilinano Osorio <mosorio@isi.edu>
2021-09-30 21:25:37 -0300

    add parameter controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,4 @@ export * from './model-parameter.controller';
 export * from './model-directive.controller';
 export * from './model-input.controller';
 export * from './model.controller';
+export * from './parameter.controller';
